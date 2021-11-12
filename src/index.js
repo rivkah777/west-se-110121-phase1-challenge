787dd2e0e5f90e1683dbccd0640864bddfeadbbf
Rebekah <rbk1008@Gmail.com>
2021-11-12 12:35:07 -0700

    Done with assigment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-// write your code here
+fetch ("http://localhost:3000/images/1")
+.then((response) => response.json())
+.then ((flataData) => flataGram(flataData));
+   
+
+function flataGram(card){
+    const name = document.querySelector("#card-title");
+   name.textContent = card.name;
+    const image = document.querySelector("#card-image");
+    image.src = card.image;
+}   
